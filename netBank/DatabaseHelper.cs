ef09ff41c5aa6e80467badd73b1fc6dda6f6b877
David <cincoisc@gmail.com>
2024-05-12 14:27:13 -0700

    databasehelper and folder Files

---
+++ netBank/DatabaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netBank
+{
+    public static class DatabaseHelper
+    {
+        private static string connectionString = @"..\Files\netBank.db";
+
+        
+
+
+    }
+}
